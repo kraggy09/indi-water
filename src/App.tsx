1ebd126161750a0f7bb130d0ce5ab802f0f8f3cf
Kaif Shaikh <kaifshaikh2013.sk@gmail.com>
2024-02-09 00:10:27 +0530

    final for the day

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="min-h-[90vh] flex-col min-w-[70vw] relative flex items-center justify-center">
       <UnderConstruction />
 
-      <div className="absolute flex items-center justify-center flex-col bottom-4 right-0">
+      <div className="md:absolute hidden md:flex items-center justify-center flex-col bottom-4 right-0">
         <img
           src={logo}
           className=" rounded-xl bg-green-400  max-w-[120px] lg:min-w-[150px] lg:min-h-[100px] max-h-[60px]"
